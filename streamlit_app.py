d80ac63bfa69c4cc678cba36d669da2f782d1a04
tripleA-A-A <abdir98@googlemail.com>
2022-12-17 15:23:42 +0000

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import pandas
+import snowflake.connector
 
-streamlit.text("Gello World")
+streamlit.header("Zenas Amazing Atheleisure Catalog")
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+def return_colours():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select COLOR_OR_STYLE from catalog_for_website")
+        return my_cur.fetchall()
+
+colours = return_colours()
+streamlit.text(type(colours))
+
+#fruits_selected = streamlit.multiselect("Pick a sweatsuit colour or style:", list(my_fruit_list.index),["Avocado","Strawberries"])
