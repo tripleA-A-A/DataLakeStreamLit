9d41ba220279c63f209a919bafeb9a285573ed59
tripleA-A-A <abdir98@googlemail.com>
2022-12-17 15:39:59 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 def return_colours():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select 'hello','goodbye' from catalog_for_website")
+        my_cur.execute("select * from catalog_for_website")
         return my_cur.fetchall()
 
-colours = ['gekl','goodvt']
+colours = return_colours()
 streamlit.dataframe(colours)
 
 
